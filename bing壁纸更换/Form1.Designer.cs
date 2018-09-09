2f76c8aac2597875bfb1ec36a9b7d3cf654a64db
郑铭午 <490804701@qq.com>
2018-09-09 19:02:44 +0800

    添加项目文件。

---
+++ bing壁纸更换/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace bing壁纸更换
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundOriginChoose = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.changeTimeChoose = new System.Windows.Forms.ComboBox();
+            this.changeNow = new System.Windows.Forms.Button();
+            this.startWithSystem = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundOriginChoose
+            // 
+            this.backgroundOriginChoose.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.backgroundOriginChoose.FormattingEnabled = true;
+            this.backgroundOriginChoose.Items.AddRange(new object[] {
+            "bing每日高清壁纸（仅限一天一换）",
+            "bing随机高清壁纸（https://bing.ioliu.cn/）"});
+            this.backgroundOriginChoose.Location = new System.Drawing.Point(61, 17);
+            this.backgroundOriginChoose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.backgroundOriginChoose.Name = "backgroundOriginChoose";
+            this.backgroundOriginChoose.Size = new System.Drawing.Size(258, 25);
+            this.backgroundOriginChoose.TabIndex = 0;
+            this.backgroundOriginChoose.SelectedIndexChanged += new System.EventHandler(this.backgroundOriginChoose_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "壁纸源";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "更换频率";
+            // 
+            // changeTimeChoose
+            // 
+            this.changeTimeChoose.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.changeTimeChoose.FormattingEnabled = true;
+            this.changeTimeChoose.Items.AddRange(new object[] {
+            "5分钟",
+            "10分钟",
+            "20分钟",
+            "30分钟",
+            "1小时",
+            "2小时",
+            "3小时"});
+            this.changeTimeChoose.Location = new System.Drawing.Point(74, 59);
+            this.changeTimeChoose.Name = "changeTimeChoose";
+            this.changeTimeChoose.Size = new System.Drawing.Size(245, 25);
+            this.changeTimeChoose.TabIndex = 3;
+            this.changeTimeChoose.SelectedIndexChanged += new System.EventHandler(this.changeTimeChoose_SelectedIndexChanged);
+            // 
+            // changeNow
+            // 
+            this.changeNow.Location = new System.Drawing.Point(12, 133);
+            this.changeNow.Name = "changeNow";
+            this.changeNow.Size = new System.Drawing.Size(156, 23);
+            this.changeNow.TabIndex = 4;
+            this.changeNow.Text = "立刻更换";
+            this.changeNow.UseVisualStyleBackColor = true;
+            this.changeNow.Click += new System.EventHandler(this.changeNow_Click);
+            // 
+            // startWithSystem
+            // 
+            this.startWithSystem.AutoSize = true;
+            this.startWithSystem.Location = new System.Drawing.Point(12, 90);
+            this.startWithSystem.Name = "startWithSystem";
+            this.startWithSystem.Size = new System.Drawing.Size(75, 21);
+            this.startWithSystem.TabIndex = 6;
+            this.startWithSystem.Text = "开机启动";
+            this.startWithSystem.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 68);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "bing壁纸更换V0.1\r\nby RF103T\r\n感谢 https://bing.ioliu.cn/\r\n提供的随机壁纸API";
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Location = new System.Drawing.Point(12, 113);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(0, 17);
+            this.state.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 165);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startWithSystem);
+            this.Controls.Add(this.changeNow);
+            this.Controls.Add(this.changeTimeChoose);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backgroundOriginChoose);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "bing壁纸更换";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox backgroundOriginChoose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox changeTimeChoose;
+        private System.Windows.Forms.Button changeNow;
+        private System.Windows.Forms.CheckBox startWithSystem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label state;
+    }
+}
+
---
+++ bing壁纸更换/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+using System.Net;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace bing壁纸更换
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);
+
+        string bingDailyPicApi = "https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1";
+        string bingRandPicApi = "https://bing.ioliu.cn/v1/rand?w=1920&h=1080";
+        bool startFlag = true;
+
+        public Form1()
+        {
+            if (!File.Exists("Settings.set"))
+            {
+                XmlDocument settings = new XmlDocument();
+                XmlDeclaration declaration = settings.CreateXmlDeclaration("1.0", "UTF-8", null);
+                settings.AppendChild(declaration);
+                XmlElement node = settings.CreateElement("设置");
+                settings.AppendChild(node);
+                XmlElement s1 = settings.CreateElement("壁纸源"), s2 = settings.CreateElement("更换频率"),s3 = settings.CreateElement("每日壁纸日期");
+                s1.SetAttribute("id", "0");
+                s2.SetAttribute("id", "1");
+                s3.SetAttribute("id", "2");
+                s1.InnerText = "0";
+                s2.InnerText = "4";
+                s3.InnerText = "";
+                node.AppendChild(s1);
+                node.AppendChild(s2);
+                node.AppendChild(s3);
+                settings.Save("Settings.set");
+            }
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument settings = new XmlDocument();
+            settings.Load("Settings.set");
+            XmlElement node = settings.DocumentElement;
+            XmlNodeList settingsList = node.ChildNodes;
+            foreach (XmlNode s in settingsList)
+            {
+                if (s.Attributes["id"].InnerText == "0")
+                    backgroundOriginChoose.SelectedIndex = int.Parse(s.InnerText);
+                if (s.Attributes["id"].InnerText == "1")
+                    changeTimeChoose.SelectedIndex = int.Parse(s.InnerText);
+            }
+            startFlag = false;
+        }
+
+        private void backgroundOriginChoose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (backgroundOriginChoose.SelectedIndex == 0)
+            {
+                changeTimeChoose.Enabled = false;
+                changeNow.Enabled = false;
+                if (!startFlag)
+                {
+                    downloadBingDaily();
+                    changeBackground(Directory.GetCurrentDirectory() + "\\temp\\dailyPic.bmp");
+                }
+            }
+            else
+            {
+                changeTimeChoose.Enabled = true;
+                changeNow.Enabled = true;
+            }
+            if (!startFlag)
+            {
+                updateSetting("0", backgroundOriginChoose.SelectedIndex.ToString());
+            }
+        }
+
+        private void changeTimeChoose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!startFlag)
+            {
+                updateSetting("1", changeTimeChoose.SelectedIndex.ToString());
+            }
+        }
+
+        private void changeNow_Click(object sender, EventArgs e)
+        {
+            downloadBingRand();
+            changeBackground(Directory.GetCurrentDirectory() + "\\temp\\randPic.bmp");
+        }
+
+        private void updateSetting(string id, string value)
+        {
+            XmlDocument settings = new XmlDocument();
+            settings.Load("Settings.set");
+            XmlElement node = settings.DocumentElement;
+            XmlNodeList settingsList = node.ChildNodes;
+            foreach (XmlNode s in settingsList)
+            {
+                if (s.Attributes["id"].InnerText == id)
+                    s.InnerText = value;
+            }
+            settings.Save("Settings.set");
+        }
+
+        private void downloadBingDaily()
+        {
+            string imageUrl = "";
+            state.Text = "正在查询更新...";
+            try
+            {
+                //准备临时Xml文件
+                Directory.CreateDirectory("temp");
+                if (File.Exists(@"temp\temp.xml"))
+                    File.Delete(@"temp\temp.xml");
+                FileStream fileStream = new FileStream(@"temp\temp.xml", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                //获取bing的Xml文件
+                HttpWebRequest getImageXml = (HttpWebRequest)WebRequest.Create(bingDailyPicApi);
+                getImageXml.ServicePoint.Expect100Continue = false;
+                getImageXml.Method = "GET";
+                getImageXml.KeepAlive = true;
+                getImageXml.ContentType = "text/xml";
+                HttpWebResponse imageXml = (HttpWebResponse)getImageXml.GetResponse();
+                using (Stream imageXmlStream = imageXml.GetResponseStream())
+                {
+                    byte[] bArr = new byte[1024];
+                    int size = imageXmlStream.Read(bArr, 0, (int)bArr.Length);
+                    while (size > 0)
+                    {
+                        fileStream.Write(bArr, 0, size);
+                        size = imageXmlStream.Read(bArr, 0, (int)bArr.Length);
+                    }
+                    fileStream.Close();
+                    imageXml.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //判断是不是最新的每日壁纸
+            XmlDocument urlXml = new XmlDocument();
+            XmlDocument settings = new XmlDocument();
+            urlXml.Load(@"temp/temp.xml");
+            settings.Load("Settings.set");
+            XmlNode node = urlXml.SelectSingleNode("//image");
+            XmlNode saveTime = settings.SelectSingleNode("//设置");
+            if ((node.SelectSingleNode("startdate")).InnerText != (saveTime.SelectSingleNode("每日壁纸日期")).InnerText)
+            {
+                //解析Xml获得每日壁纸地址
+                node = urlXml.SelectSingleNode("//image");
+                if (node != null)
+                    imageUrl = "http://www.bing.com" + (node.SelectSingleNode("urlBase")).InnerText + "_1920x1080.jpg";
+                //下载图片
+                downloadPic(imageUrl, @"temp/dailyPic.bmp");
+                (saveTime.SelectSingleNode("每日壁纸日期")).InnerText = (node.SelectSingleNode("startdate")).InnerText;
+            }
+            else
+                state.Text = "";
+            urlXml.Save(@"temp/temp.xml");
+            settings.Save("Settings.set");
+            File.Delete(@"temp/temp.xml");
+        }
+
+        private void downloadBingRand()
+        {
+            state.Text = "正在下载壁纸...";
+            try
+            {
+                FileStream fs = new FileStream(@"temp/temp_1.jpg", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(bingRandPicApi);
+                request.Credentials = CredentialCache.DefaultCredentials;
+                request.Accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*";
+                request.Headers.Add("Accept-Language", "zh-CN");
+                request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1)";
+                request.ContentType = "application/x-www-form-urlencoded";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                byte[] bArr = new byte[1024];
+                int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                while (size > 0)
+                {
+                    fs.Write(bArr, 0, size);
+                    size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                }
+                fs.Close();
+                responseStream.Close();
+                JPGtoBMP(@"temp/temp_1.jpg", @"temp/randPic.bmp");
+                File.Delete(@"temp/temp_1.jpg");
+                state.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void downloadPic(string url, string path)
+        {
+            state.Text = "正在下载壁纸...";
+            try
+            {
+                HttpWebRequest getImage = (HttpWebRequest)WebRequest.Create(url);
+                getImage.ServicePoint.Expect100Continue = false;
+                getImage.Method = "GET";
+                getImage.KeepAlive = true;
+                getImage.ContentType = "image/*";
+                HttpWebResponse image = (HttpWebResponse)getImage.GetResponse();
+                Stream stream = null;
+                stream = image.GetResponseStream();
+                Image.FromStream(stream).Save(@"temp/temp.jpg");
+                JPGtoBMP(@"temp/temp.jpg", path);
+                stream.Dispose();
+                stream.Close();
+                image.Close();
+                File.Delete(@"temp/temp.jpg");
+                state.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void JPGtoBMP(string jpgFile, string bmpFile)
+        {
+            //转换jpg到bmp
+            using (Bitmap source = new Bitmap(jpgFile))
+            {
+                using (Bitmap bmp = new Bitmap(source.Width, source.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb))
+                {
+                    Graphics.FromImage(bmp).DrawImage(source, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                    bmp.Save(bmpFile, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+            }
+        }
+
+        private void changeBackground(string bmpFile)
+        {
+            SystemParametersInfo(20, 0, bmpFile, 0x2);
+        }
+    }
+}
